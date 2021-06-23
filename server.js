f9e7cee150168ba7add72fe9a9ef7ca6ca489cb3
yashsharma8415 <yashsharma8415@gmail.com>
2021-06-23 23:32:35 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,77 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
+const path = require("path");
+const expressLayout = require("express-ejs-layouts");
+const PORT = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+const session = require("express-session");
+const flash = require("express-flash");
+const MongoDbStore = require("connect-mongo");
+const passport = require("passport");
+
+// Database connection
+const url = "mongodb://localhost/pizza";
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: true,
+});
+const connection = mongoose.connection;
+connection
+  .once("open", () => {
+    console.log("Database connected...");
+  })
+  .catch((err) => {
+    console.log("Connection failed...");
+  });
+
+//Session Store
+let mongoStore = MongoDbStore.create({
+  mongoUrl: url,
+  collection: "sessions",
+});
+
+//Session Config
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: mongoStore,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 }, // 24 hour
+  })
+);
+
+// Passport config
+const passportInit = require("./app/config/passport");
+passportInit(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
+// Assets
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Global middleware
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  res.locals.user = req.user;
+  next();
+});
+
+// set template engine
+app.use(expressLayout);
+app.set("views", path.join(__dirname, "/resources/views"));
+app.set("view engine", "ejs");
+
+require("./routes/web")(app);
+
+app.listen(PORT, () => {
+  console.log(`Listening on the port ${PORT}`);
+});
